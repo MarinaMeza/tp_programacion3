055ad50c7be510d56ff770887db02cba00f40d29
MarinaMeza <kaiodelnorte@gmail.com>
2017-03-31 21:02:22 -0300

    Trabajo práctico Parte 1 Segundo commit

--- empleado.php
+++ empleado.php
@@ -1,5 +1,36 @@
 <?php
 
+    class Empleado extends Persona
+    {
+        protected $_legajo;
+        protected $_sueldo;
 
+        public function __construct($pNombre,$pApellido,$pDni,$pSexo,$pLegajo,$pSueldo)
+        {
+            parent::__construct($pApellido,$pDni,$pNombre,$pSexo);
+            $this->_legajo=$pLegajo;
+            $this->_sueldo=$pSueldo;
+        }
+
+        public function getLegajo()
+        {
+            return $this->_legajo;
+        }
+
+        public function getSueldo()
+        {
+            return $this->_sueldo;
+        }
+
+        public function Hablar()
+        {
+
+        }
+
+        public function ToString()
+        {
+            return parent::ToString()." - $this->_legajo - $this->_sueldo <br>";
+        }
+    }
 
 ?>
--- persona.php
+++ persona.php
@@ -1,6 +1,49 @@
 <?php
     
+    abstract class Persona
+    {
+        private $_apellido;
+        private $_dni;
+        private $_nombre;
+        private $_sexo;
 
+        public function __construct($pApellido,$pDni,$pNombre,$pSexo)
+        {
+            $this->_apellido=$pApellido;
+            $this->_dni?$pDni;
+            $this->_nombre=$pNombre;
+            $this->_sexo=$pSexo;
+        }
+
+        public function getApellido()
+        {
+            return $this->_apellido;
+        }
+
+        public function getDni()
+        {
+            return $this->_dni;
+        }
+
+        public function getNombre()
+        {
+            return $this->_nombre;
+        }
+
+        public function getSexo()
+        {
+            return $this->_sexo;
+        }
+
+        public abstract function Hablar($pIdioma)
+        {
+
+        }
+
+        public function ToString()
+        {
+            return "$this->_nombre - $this->_apellido - $this->_edad - $this->_sexo <br>";
+        }
     }
 
 
