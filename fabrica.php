a6662599ac3a7f3f803fa72e4f61b620369df9fd
alumno <alumno>
2017-04-03 19:38:20 -0300

    Trabajo práctico Parte 1 Quinto commit

--- fabrica.php
+++ fabrica.php
@@ -1,4 +1,50 @@
 <?php
 
+    class Fabrica
+    {
+        private $_empleados;
+        private $_razonSocial;
+
+        public function __construct($pRazonSocial)
+        {
+            $this->_razonSocial=$pRazonSocial;
+            $this->_empleados=array();
+        }
+
+        public function AgregarEmpleado($pEmpleado)
+        {
+            array_push($this->_empleado,$pEmpleado);
+        }
+
+        public function CalcularSueldos()
+        {
+            
+        }
+
+        public function EliminarEmpleado($pPersona)
+        {
+            for($i=0;$i<count($this->_empleado);$i++)
+            {
+                if($this->_empleado[$i]->getLegajo()==$pPersona[$i]->getLegajo())
+                {
+                    unset($this->empleado[$i]);
+                }
+            }
+        }
+
+        private function EliminarEmpleadosRepetidos()
+        {
+
+        }
+
+        public function ToString()
+        {
+
+            foreach ($this->_emplados as $key)
+            {
+                echo $key->ToString();
+                echo " - ";
+            }
+        }
 
 ?>
--- index.php
+++ index.php
@@ -2,10 +2,14 @@
 
     require_once "persona.php";
     require_once "empleado.php";
-
+    require_once "fabrica.php";
     $nuevoEmpleado = new Empleado("Marina","Meza",123456789,'F',1999,50000);
 
     echo $nuevoEmpleado->ToString();
     echo $nuevoEmpleado->Hablar(" Ingles");
+
+    $miFabrica = new Fabrica("FabricaSA");
+    echo $miFabrica->ToString();
+    
 
 ?>
