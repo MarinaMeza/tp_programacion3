1bbb92265981db4dbed4b4ee3ee5cfaa48a7a3bf
MarinaMeza <kaiodelnorte@gmail.com>
2017-04-10 00:34:21 -0300

    Trabajo práctico Parte 2 Octavo commit

--- administracion.php
+++ administracion.php
@@ -1,5 +1,13 @@
 <?php
+    require_once "empleado.php";
 
-    echo "HOLA";
+    //var_dump($_POST);
 
+    $pruebaEmpleado=new Empleado($_POST["nombre"],$_POST["apellido"],$_POST["dni"],$_POST["sexo"],$_POST["legajo"],$_POST["sueldo"]);
+
+    $fichero=fopen("empleados.txt","a");
+    fwrite($fichero,$pruebaEmpleado->ToString());
+    fwrite($fichero,"\r\n");
+
+    echo $pruebaEmpleado->ToString();
 ?>
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-    require_once "persona.php";
+    
     require_once "empleado.php";
     require_once "fabrica.php";
     $nuevoEmpleado1 = new Empleado("Marina","Meza",123456789,'F',1999,55000);
